63e8fb30bf85a9b9f78976d1b43a3800af8d9af4
NicolasLupke <nicolas.leandro.lupke@gmail.com>
2025-12-21 17:00:54 +0100

    extract tasks from bpmn

--- Next-Activity-Prediction/bpmn_parser.py
+++ Next-Activity-Prediction/bpmn_parser.py
@@ -6,10 +6,18 @@ class BPMNParser:
         self.tree = ET.parse(bpmn_path)
         self.root = self.tree.getroot()
         self.ns = self._detect_namespace()
+        self.tasks = {}
+        self._parse_tasks()
 
     def _detect_namespace(self):
         tag = self.root.tag
         if tag.startswith('{'):
             return tag[1:tag.index('}')]
         return 'http://www.omg.org/spec/BPMN/20100524/MODEL'
+
+    def _parse_tasks(self):
+        for task in self.root.iter(f'{{{self.ns}}}task'):
+            task_id = task.get('id')
+            task_name = task.get('name', task_id)
+            self.tasks[task_id] = task_name
 
