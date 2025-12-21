32381ec9b24b5a4b9eece40037fb00805c7bc7de
NicolasLupke <nicolas.leandro.lupke@gmail.com>
2025-12-21 17:28:04 +0100

    extract trace activities

--- Next-Activity-Prediction/log_analyzer.py
+++ Next-Activity-Prediction/log_analyzer.py
@@ -7,3 +7,14 @@ class LogAnalyzer:
         if hasattr(self.log, 'iterrows'):
             self.log = pm4py.convert_to_event_log(self.log)
 
+    def get_trace_activities(self, trace):
+        activities = []
+        for event in trace:
+            if 'concept:name' in event:
+                activities.append(event['concept:name'])
+        return activities
+
+    def iter_traces(self):
+        for trace in self.log:
+            yield self.get_trace_activities(trace)
+
